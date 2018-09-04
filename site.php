5de9ad4eadf83ff9100366662b0c3ae484cbda08
Patrick Chagas <patrickchagas21@gmail.com>
2018-09-04 19:19:38 -0300

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -72,7 +72,8 @@ $app->get('/cart', function() {
 
 	$page->setTpl('cart', [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 });
@@ -125,6 +126,18 @@ $app->get('/cart/:idproduct/remove' , function($idproduct) {
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+//Rota pra enviar o CEP pra calcular o frete
+$app->post('/cart/freight', function() {
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit;
