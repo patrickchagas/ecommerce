e8f357bbe3253f98f5421b4e9c52db3bdd81b51c
Patrick Chagas <patrickchagas21@gmail.com>
2018-09-05 23:25:42 -0300

    Meus Pedidos E Resumo Carrinho

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 //Formatar em reais
 function formatPrice($vlprice)
@@ -24,6 +25,26 @@ function getUserName()
 
 	return $user->getdesperson();
 
+}
+
+function getCartNrQtd()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+}
+
+function getCartVlSubTotal()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
 }
 
 ?>
