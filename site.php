4a76f1c427eb70bc3f19a11f549bfa4e3166646c
Patrick Chagas <patrickchagas21@gmail.com>
2018-09-07 19:56:48 -0300

    Integração Paypal HTML

--- site.php
+++ site.php
@@ -266,7 +266,17 @@ $app->post('/checkout', function() {
 
 	$order->save();
 
-	header("Location: /order/".$order->getidorder()."/pagseguro");
+	switch ((int)($_POST['payment-method'])) {
+
+		case 1:
+		header("Location: /order/".$order->getidorder()."/pagseguro");
+		break;
+
+		case 2:
+		header("Location: /order/".$order->getidorder()."/paypal");
+		break;
+	}
+
 	exit;
 
 });
@@ -295,6 +305,34 @@ $app->get('/order/:idorder/pagseguro', function($idorder) {
 			'areaCode'=>substr($order->getnrphone(), 0, 2),
 			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
 		]
+	]);
+
+	// header ("Location: /order/".$order->getidorder());
+ 
+	// exit;
+
+});
+
+$app->get('/order/:idorder/paypal', function($idorder) {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+
+	$page->setTpl("payment-paypal", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
 	]);
 
 	// header ("Location: /order/".$order->getidorder());
