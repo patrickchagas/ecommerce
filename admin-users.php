2d414cb0e5ea2c7ccf66068b714be6e0d44c514e
Patrick Chagas <patrickchagas21@gmail.com>
2018-09-07 00:40:56 -0300

    Admin - User - Search And Pagination

--- admin-users.php
+++ admin-users.php
@@ -9,12 +9,39 @@ $app->get("/admin/users", function() {
 	//Verificar se o usuário está logado
 	User::verifyLogin();
 
-	// Array com toda lista de usuário
-	$users = User::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "" ;
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if($search != ''){
+
+		$pagination = User::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = User::getPage($page);
+
+	}
+
+	
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++) { 
+		array_push($pages, [
+			'href'=>'/admin/users?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+	}
 
 	$page = new PageAdmin();
+
 	$page->setTpl("users", array( //Passar a lista para o template
-		"users"=>$users
+		"users"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
 	));
 
 });
